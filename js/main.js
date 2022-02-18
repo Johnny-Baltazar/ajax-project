047e6ada9e1945c5915e2ae39ce793620afa995d
Johnny Baltazar <baltazarjohnny@yahoo.com>
2022-02-17 20:08:58 -0800

    User can view a list of engines. #1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 var $enginesLink = document.querySelector('.engines-link');
+var $subaruSti = document.querySelector('.subaru-sti');
+var $integraGsr = document.querySelector('.integra-gsr');
+var $civicSi = document.querySelector('.civic-si');
+var $chevy = document.querySelector('.chevy');
 
 $enginesLink.addEventListener('click', function (event) {
-  // console.log('clicked');
+
+});
+
+$subaruSti.addEventListener('click', function (event) {
+
+});
+
+$integraGsr.addEventListener('click', function (event) {
+
+});
+
+$civicSi.addEventListener('click', function (event) {
+
+});
+
+$chevy.addEventListener('click', function (event) {
+
 });
