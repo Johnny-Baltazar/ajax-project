6cb62e647c7e5ce77bbbd8fc8fca0c70df27859f
Johnny Baltazar <baltazarjohnny@yahoo.com>
2022-02-17 23:20:37 -0800

    add a sibling  element in the HTML. #2

--- js/data.js
+++ js/data.js
@@ -1,1 +1,7 @@
 /* exported data */
+var data = {
+  view: 'engine-applications',
+  entries: [],
+  editing: null,
+  nextEntryId: 1
+};
--- js/main.js
+++ js/main.js
@@ -3,13 +3,18 @@ var $subaruSti = document.querySelector('.subaru-sti');
 var $integraGsr = document.querySelector('.integra-gsr');
 var $civicSi = document.querySelector('.civic-si');
 var $chevy = document.querySelector('.chevy');
+var $subaruPage = document.querySelector('.subaru-page');
+var $engineApplications = document.querySelector('.engine-applications');
 
 $enginesLink.addEventListener('click', function (event) {
 
 });
 
 $subaruSti.addEventListener('click', function (event) {
+  event.preventDefault();
 
+  $subaruPage.classList.remove('hidden');
+  $engineApplications.classList.add('hidden');
 });
 
 $integraGsr.addEventListener('click', function (event) {
