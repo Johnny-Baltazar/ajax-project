d5b619a135b3916f3c55f9431d0a98e9849cea43
Johnny Baltazar <baltazarjohnny@yahoo.com>
2022-04-22 08:06:09 -0700

    Ensure that a VIN can be decoded thru the NHTSA API. #3

--- js/main.js
+++ js/main.js
@@ -70,14 +70,44 @@ $chevy.addEventListener('click', function (event) {
 
 const letterNumber = /^[0-9a-zA-Z]+$/;
 
-$searchOne.addEventListener('click', event => {
-  const vinInput = $vinOne.value;
+// const $vinDecode = document.querySelector('#vin-decode');
+const $vinModal = document.querySelector('.vin-modal');
+
+const returnedVinDecode = vin => {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/' + vin + '?format=json');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    // console.log('Status1:', xhr.status);
+    // console.log('response:', xhr.response);
+
+    // create an object, and fill the object with the values from the VIN decoder api as key value pairs.
+    var results = xhr.response.Results;
+    for (var key in results) {
+      // console.log('results[key].Value:', results[key].Value);
+      if (results[key].Variable === 'Make') {
+        // console.log('Make:', results[key].Value);
+      }
+    }
+  });
+  xhr.send();
+
+};
+
+$searchOne.addEventListener('click', event => { // Subaru sample VIN: JF1GD70644L519076
+  var vinInput = $vinOne.value; // GSR sample VIN: JH4DC2390XS004187
+
   for (var i = 0; i < vinInput.length; i++) {
     if (vinInput.length !== 17) {
       $notSeventeen.classList.remove('hidden');
-    } else if (vinInput[i] !== letterNumber) {
+    } else if (!/^[0-9a-zA-Z]+$/.test(vinInput)) {
       $notAplhanum.classList.remove('hidden');
+    } else {
+      returnedVinDecode(vinInput);
+      $subaruPage.classList.add('hidden');
+      $vinModal.classList.remove('hidden');
     }
+
   }
 });
 
@@ -98,7 +128,7 @@ $searchTwo.addEventListener('click', event => {
 const $vinThree = document.querySelector('.vin-three');
 const $searchThree = document.querySelector('.search-three');
 
-$searchThree.addEventListener('click', event => {
+$searchThree.addEventListener('click', event => { // GSR sample VIN: JH4DC2390XS004187
   const vinInput = $vinThree.value;
   for (var i = 0; i < vinInput.length; i++) {
     if (vinInput.length !== 17) {
